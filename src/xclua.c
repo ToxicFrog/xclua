57b67335fbb86aec77a662cbaba4b2d07727bcd8
Ben Kelly <ben@orias.ancilla.ca>
2009-04-16 11:27:16 -0400

    Fixed __export, gitignore

--- src/xclua.c
+++ src/xclua.c
@@ -9,18 +9,17 @@ xchat_plugin * ph;	/* plugin handle */
 /*	============================================================================
 		Entry Points
 ============================================================================  */
-int __export xchat_plugin_init(
+int xchat_plugin_init(
 				xchat_plugin *plugin_handle,
 				char **plugin_name,
 				char **plugin_desc,
 				char **plugin_version,
 				char *arg)
-{
+{                             
     return 1;
 }
 
-int __export xchat_plugin_deinit()
+int xchat_plugin_deinit()
 {
 	return 1;
 }
-
